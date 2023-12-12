2bb8f38df71c49ba8ee98ff128e79ecdf78b69d3
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2023-12-12 18:24:54 +0100

    day1 part2

--- day1/main.c
+++ day1/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+const char* numbers[] = {
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+};
+
+const int num_lens[] = {
+    3, 3, 5, 4, 4, 3, 5, 5, 4
+};
+
+int get_num(char* buf, int pos, int len)
+{
+    if (buf[pos] <= '9' && buf[pos] >= '0') {
+        return (buf[pos] - '0');
+    }
+    for (int j = 0; j < 9; j++) {
+        if (len-pos >= num_lens[j] && strncmp(buf+pos, numbers[j], num_lens[j]) == 0) {
+            return j+1;
+        }
+    }
+    return -1;
+}
+
 int main(void)
 {
     char buf[1024] = {0};
@@ -8,15 +29,19 @@ int main(void)
     int next;
     while (scanf("%1024s", buf) != EOF) {
         next = 0;
-        for (int i = 0; i < 1024 && buf[i] != '\0'; i++) {
-            if (buf[i] <= '9' && buf[i] >= '0') {
-                next += (buf[i] - '0')*10;
+        int len = strlen(buf);
+        for (int i = 0; i < len; i++) {
+            int num = get_num(buf, i, len);
+            if (num != -1) {
+                next = num*10;
                 break;
-            }   
+            }
         }
-        for (int i=strlen(buf)-1; i >= 0; --i) {
-            if (buf[i] <= '9' && buf[i] >= '0') {
-                next += (buf[i] - '0');
+        printf("next: %d\n", next);
+        for (int i=len-1; i >= 0; --i) {
+            int num = get_num(buf, i, len);
+            if (num != -1) {
+                next += num;
                 break;
             }
         }
