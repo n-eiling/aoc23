55808f9fe8fa4a15689ee3dbe5b8e18fd82043cb
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2023-12-12 19:55:13 +0100

    day2 part2

--- day2/main.c
+++ day2/main.c
@@ -12,12 +12,14 @@ int main(void)
     size_t buflen = 0;
     int gameid = 0;
     int sum = 0;
+    int powersum = 0;
     int len = 0;
     char* group_begin;
     char* next_group_begin;
     while (getline(&buf, &buflen, stdin) != EOF) {
         int works = 1;
-        printf("buf: %s\n", buf);
+        int max_colors[3] = {0};
+        printf("buf: %s", buf);
         if (sscanf(buf, "Game %d:", &gameid) == EOF) {
             printf("sscanf error\n");
             return -1;
@@ -42,6 +44,9 @@ int main(void)
                 for (int j = 0; j < 3; j++) {
                     if (strncmp(color, colors[j], colors_lens[j]) == 0) {
                         cnums[j] += num;
+                        if (num > max_colors[j]) {
+                            max_colors[j] = num;
+                        }
                         break;
                     }
                 }
@@ -49,7 +54,6 @@ int main(void)
             }
             if (cnums[0] > max_nums[0] || cnums[1] > max_nums[1] || cnums[2] > max_nums[2]) {
                 works = 0;
-                break;
             }
             printf ("red %d, blue %d, green %d\n", cnums[0], cnums[1], cnums[2]);
             group_begin = next_group_begin;
@@ -62,6 +66,10 @@ int main(void)
         if (works) {
             sum += gameid;
         }
+        int power = max_colors[0] * max_colors[1] * max_colors[2];
+        printf("power: %d\n\n", power);
+        powersum += power;
     }
     printf("sum: %d\n", sum);
+    printf("powersum: %d\n", powersum);
 }
